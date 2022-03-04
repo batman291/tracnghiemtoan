094d2e7cc2f58bcab8aa24251488064c83ac6ef1
bat <94910737+batman291@users.noreply.github.com>
2022-03-04 21:00:06 +0700

    Create min.js

---
+++ min.js
@@ -0,0 +1,24 @@
+
+<div class="quiz-main" data-duration="0" data-quiztype="Graded"> 
+<ul> 
+ <li class="quiz-section" data-sectionid=""><div class="quiz-section-title"><b><div class="separator" style="clear: both; text-align: center;"><b style="text-align: left;"><div class="separator" style="clear: both; text-align: center;"><a href="https://blogger.googleusercontent.com/img/a/AVvXsEhCJul3DqYP9g9pg6jU1bEBeueclMqBEQRM2hKy-zqi0SV4cBAIbvOblMV0S-P29BsBf9FjuO2a1dSWQUyKCB-0IxIQgmhSXd25zRBQIIpF5GSvbYZNCMyk-Ahq2roaKDs_97Ys1yaaE3u3OwLZzlRviJym2DBczJmH5IifdWiWgldPcL7r6vfJyF1b_g=s559" imageanchor="1" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="284" data-original-width="559" height="298" src="https://blogger.googleusercontent.com/img/a/AVvXsEhCJul3DqYP9g9pg6jU1bEBeueclMqBEQRM2hKy-zqi0SV4cBAIbvOblMV0S-P29BsBf9FjuO2a1dSWQUyKCB-0IxIQgmhSXd25zRBQIIpF5GSvbYZNCMyk-Ahq2roaKDs_97Ys1yaaE3u3OwLZzlRviJym2DBczJmH5IifdWiWgldPcL7r6vfJyF1b_g=w548-h298" width="548" /></a></div><br /><span style="color: black; font-family: Times, Times New Roman, serif;"><b style="font-family: -webkit-standard;"><div class="separator" style="clear: both; text-align: center;"><b style="text-align: left;"><span style="color: #6aa84f; font-family: Times, Times New Roman, serif; font-size: medium;">- Cách nhận biết bạn chọn đúng hay sai:</span></b></div><div class="separator" style="clear: both; text-align: center;"><b style="text-align: left;"><span style="color: #6aa84f; font-family: Times, Times New Roman, serif; font-size: medium;">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;+ Khi nhấn kết quả đáp án của bạn chọn hiện màu xanh là đúng.</span></b></div><div class="separator" style="clear: both; text-align: center;"><b style="text-align: left;"><span style="color: #6aa84f; font-family: Times, Times New Roman, serif; font-size: medium;">&nbsp; + Khi nhấn kết quả mà không hiện màu gì thì đó là câu sai.</span></b></div></b></span></b></div><div class="separator" style="clear: both; text-align: center;"><b style="text-align: left;"><span style="color: black; font-family: Times, Times New Roman, serif;"><br /></span></b></div><div class="separator" style="clear: both; text-align: center;"><b style="text-align: left;"><span style="color: black; font-family: Times, Times New Roman, serif;">Câu 1 : </span></b><b style="text-align: left;"><span style="color: black; font-family: Times, Times New Roman, serif;">He hoped the appointment would enable him to gain greater …………. in publishing.</span></b></div></b></div>
+<div class="quiz-section-content">
+<div class="item item-question item-choices">
+<ul class="list btn-group group-theme">
+<li class="list-item"><label class="overlabel"><input class="hide" data-point="1" data-status="1" name="option-" type="radio" /> <span class="mark"></span></label>
+<div class="boxcheck">
+<div class="answer-desc textview"> Experience </div>
+ </div></li>
+<li class="list-item"><label class="overlabel"><input class="hide" data-point="0" data-status="0" name="option-" type="radio" /> <span class="mark"></span></label>
+<div class="boxcheck">
+<div class="answer-desc textview"> Word </div>
+ </div></li>
+ <li class="list-item"><label class="overlabel"><input class="hide" data-point="0" data-status="0" name="option-" type="radio" /> <span class="mark"></span> </label>
+<div class="boxcheck">
+<div class="answer-desc textview"> Job </div>
+ </div></li>
+<li class="list-item"><label class="overlabel"><input class="hide" data-point="0" data-status="0" name="option-" type="radio" /> <span class="mark"></span> </label>
+<div class="boxcheck">
+<div class="answer-desc textview">Employment</div>
+ </div></li>
+ </ul></div></div></li><br />
